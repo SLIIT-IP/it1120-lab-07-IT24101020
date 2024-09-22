2225065d0804b15b66e42fce51a15f36dd36df56
IT24101020 <it24101020@my.sliit.lk>
2024-09-22 20:50:29 +0530

    Add files via upload

---
+++ Q2/IT24101020Lab7Q2A.java
@@ -0,0 +1,8 @@
+public class IT24101020Lab7Q2A {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println("$ $ $ $ $");
+        }
+    }
+}
---
+++ Q2/IT24101020Lab7Q2B.java
@@ -0,0 +1,12 @@
+public class IT24101020Lab7Q2B {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + " - ");
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
